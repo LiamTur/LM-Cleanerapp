3c52f5dc32388b2e8158b58e8a94f44fbd39d6f4
Liam Turelid <turelid@chalmers.se>
2025-06-03 17:34:14 +0000

    first commit, login screen kinda done add browser routing and login variable and then voula

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       {/* <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h1>LM-cleanapp log in:</h1>
+     <div className="card">
+	<button>Liam</button> <button>Moa</button>
+      </div>
+    </>
+  )
+}
+
+export default App
